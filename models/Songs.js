f6e919741040e09825d4527dcdf89751fb055f0e
amos-kibet <kibetamos97@gmail.com>
2022-09-11 14:45:01 +0300

    tmp-fix: comment out unnecessary code

--- models/Songs.js
+++ models/Songs.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
+// import dotenv from "dotenv";
 
-dotenv.config();
+// dotenv.config();
 
 const songsSchema = new mongoose.Schema({
   trackName: {
