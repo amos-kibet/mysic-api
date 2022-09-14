8ccb5caf76a2e9ad14b20d4de417ef85320c3c63
amos-kibet <kibetamos97@gmail.com>
2022-09-14 12:21:56 +0300

    fix:🔨 remove required from username field

--- models/User.js
+++ models/User.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+  username: { type: String },
   email: { type: String, required: true },
   password: { type: String, require: true },
 });
