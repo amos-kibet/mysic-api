2f23f10032eb77f49edc0b7895886f16fece2021
amos-kibet <kibetamos97@gmail.com>
2022-09-17 16:51:32 +0300

    fix: adjust users model to include confirmedEmail field

--- models/User.js
+++ models/User.js
@@ -4,6 +4,7 @@ const userSchema = new mongoose.Schema({
   username: { type: String },
   email: { type: String, required: true },
   password: { type: String, require: true },
+  confirmedEmail: { type: Boolean, default: false },
 });
 
 export const User = mongoose.model("Users", userSchema);
