1466b69ddee2a3d3045f8564d7ec3dfe89484e2b
sankaire <kerenketepela@gmail.com>
2022-09-18 17:09:46 +0300

    feat:🔒 create token and send it on headers

--- controllers/auth.js
+++ controllers/auth.js
@@ -4,10 +4,13 @@
 import { User } from "../models/User.js";
 import { hash, compare } from "../utils/password.js";
 import { sendConfirmationEmail, confirmedEmail } from "../utils/email.js";
+import Jwt from "jsonwebtoken";
+import cookie from 'cookie'
 
-export const signUpController = async (req, res) => {
+
+export const signUpController = (req, res) => {
   const { username, email, password } = req.body;
-  await User.findOne({ email }, (err, data) => {
+  User.findOne({ email }, async (err, data) => {
     if (err) {
       return res.status(500).json({
         error: `Server error message 1: ${err.message}`,
@@ -16,31 +19,38 @@ export const signUpController = async (req, res) => {
     if (data) {
       return res.status(401).json({
         error: "Email already in use",
-      });
+      })
     }
 
     const user = new User({
       username: username,
       email: email,
       password: hash(password),
-      confirmedEmail: confirmedEmail(),
+
     });
 
-    user
+    await user
       .save()
-      .then(() => {
-        return res.status(201).json({
-          success: "User created successfully",
-          // alternate place to send email confirmation
-        });
+      .then((data) => {
+        const token = Jwt.sign({ id: data._id }, 'dhfrurfeufe');
+        res.setHeader(
+          "Set-Cookie",
+          cookie.serialize("mysic_acces_token", token, {
+            httpOnly: true,
+            maxAge: 2 * 60 * 60,
+            path: "/",
+            // sameSite: "mysic",
+            secure: process.env.NODE_ENV === "production",
+          })
+        )
+        sendConfirmationEmail(email, user._id);
+        res.json(data)
       })
       .catch((err) => {
         return res.status(500).json({
           error: `Server error message 2: ${err.message}`,
         });
       });
-    // sends confirmation code to user's email
-    sendConfirmationEmail(email);
   }).clone();
 };
 
