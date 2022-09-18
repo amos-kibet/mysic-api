857e8ab187697a98dab868393439c38ca2f6505c
sankaire <kerenketepela@gmail.com>
2022-09-18 17:17:46 +0300

    fix:🚨 pass on params instead of query

--- routes/user.js
+++ routes/user.js
@@ -1,11 +1,12 @@
 import express from "express";
 
 import { signUpController, signInController } from "../controllers/auth.js";
+import { confirmedEmail } from "../utils/email.js";
 
 const router = express.Router();
 
 router.post("/signup", signUpController).post("/signin", signInController);
-router.get("/confirm/?:id");
+router.get("/confirm/:id", confirmedEmail)
 router.post("/signin");
 
 export const authRouter = router;
