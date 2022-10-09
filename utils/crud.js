4d206d525f6030b7e819eae4526016e4daa9fd67
amos-kibet <kibetamos97@gmail.com>
2022-10-09 15:27:22 +0300

    clean up working tree

--- utils/crud.js
+++ utils/crud.js
@@ -1,32 +1,30 @@
-import {logger} from "./log.js";
+import { logger } from "./log.js";
 
+export const deleteDocument = (model) => async (req, res) => {
+  const { userID } = req.params;
 
-export const deleteDocument = model => async (req, res) => {
-    const { userID } = req.params
-
-    try {
-        const doc = await model.findOneAndDelete({ _id: userID }).exec()
-        if (!doc) {
-            logger.error('Could not find document')
-            return res.status(204).json({ message: 'user does not exist' })
-        }
-        return res.status(200).json(doc)
-
-    } catch (error) {
-        logger.error(error.message)
-        return res.status(500).json(error.message)
+  try {
+    const doc = await model.findOneAndDelete({ _id: userID }).exec();
+    if (!doc) {
+      logger.error("Could not find document");
+      return res.status(204).json({ message: "Document does not exist" });
     }
-}
-export const getAllDocuments = model => async (req, res) => {
-    try {
-        const data = await model.find({}).exec();
-        if (!data) {
-            logger.error('Could not find document')
-            return res.status(500).json({ message: "no songs found" });
-        }
-        return res.status(200).json({ data });
-    } catch (error) {
-        logger.error(error.message)
-        return res.status(500).json({ message: error.message });
+    return res.status(200).json(doc);
+  } catch (error) {
+    logger.error(error.message);
+    return res.status(500).json(error.message);
+  }
+};
+export const getAllDocuments = (model) => async (req, res) => {
+  try {
+    const data = await model.find({}).exec();
+    if (!data) {
+      logger.error("Could not find document");
+      return res.status(500).json({ message: "No document found" });
     }
+    return res.status(200).json({ data });
+  } catch (error) {
+    logger.error(error.message);
+    return res.status(500).json({ message: error.message });
+  }
 };
--- utils/log.js
+++ utils/log.js
@@ -1,21 +1,21 @@
 import winston from "winston";
-const timestamp = new Date(Date.now()).toLocaleString("en-us",{
-    timeZone:"Africa/Nairobi"
-})
+const timestamp = new Date(Date.now()).toLocaleString("en-us", {
+  timeZone: "Africa/Nairobi",
+});
 const customFormat = winston.format.combine(
-    winston.format.printf((info) => {
-        return `${timestamp} [${info.level.toUpperCase().padEnd(7)}]: ${
-            info.message
-        }`;
-    })
+  winston.format.printf((info) => {
+    return `${timestamp} [${info.level.toUpperCase().padEnd(7)}]: ${
+      info.message
+    }`;
+  })
 );
 export const logger = winston.createLogger({
-    format: customFormat,
-    transports: [
-        new winston.transports.File({
-            filename: "./logs/error.log",
-            level: "error",
-        }),
-        new winston.transports.File({ filename: "./logs/info.log", level: "info" }),
-    ],
+  format: customFormat,
+  transports: [
+    new winston.transports.File({
+      filename: "./logs/errors.log",
+      level: "error",
+    }),
+    new winston.transports.File({ filename: "./logs/info.log", level: "info" }),
+  ],
 });
