85885c80b95048f09944689839e25f750110ea89
amos-kibet <kibetamos97@gmail.com>
2022-10-10 17:42:09 +0300

    fix:🔧 debug logger file implementation

--- utils/crud.js
+++ utils/crud.js
@@ -6,12 +6,12 @@ export const deleteDocument = (model) => async (req, res) => {
   try {
     const doc = await model.findOneAndDelete({ _id: userID }).exec();
     if (!doc) {
-      logger.error("Could not find document");
+      logger.log("error", "Could not find document");
       return res.status(204).json({ message: "Document does not exist" });
     }
     return res.status(200).json(doc);
   } catch (error) {
-    logger.error(error.message);
+    logger.log("error", error.message);
     return res.status(500).json(error.message);
   }
 };
@@ -19,12 +19,12 @@ export const getAllDocuments = (model) => async (req, res) => {
   try {
     const data = await model.find({}).exec();
     if (!data) {
-      logger.error("Could not find document");
+      logger.log("error", "Could not find document");
       return res.status(500).json({ message: "No document found" });
     }
     return res.status(200).json({ data });
   } catch (error) {
-    logger.error(error.message);
+    logger.log("error", error.message);
     return res.status(500).json({ message: error.message });
   }
 };
