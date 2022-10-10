4a47a84cbb6fec6bf83704924738fd46220b876d
amos-kibet <kibetamos97@gmail.com>
2022-10-10 17:42:15 +0300

    fix:🔧 debug logger file implementation

--- utils/log.js
+++ utils/log.js
@@ -1,4 +1,8 @@
 import winston from "winston";
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+
 const timestamp = new Date(Date.now()).toLocaleString("en-us", {
   timeZone: "Africa/Nairobi",
 });
@@ -19,3 +23,9 @@ export const logger = winston.createLogger({
     new winston.transports.File({ filename: "./logs/info.log", level: "info" }),
   ],
 });
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+export const httpLogStream = fs.createWriteStream(
+  path.join(__dirname, "../", "logs", "http_logs.log")
+);
